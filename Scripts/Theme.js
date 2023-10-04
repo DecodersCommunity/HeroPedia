41bc2f424efcf0f87d9728f32dc7676e3079c9e6
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2023-10-04 23:22:18 +0530

    Update Theme.js

--- Scripts/Theme.js
+++ Scripts/Theme.js
@@ -1,14 +1,18 @@
 // Function to toggle dark mode
 function toggleDarkMode() {
+    // Get references to elements you want to toggle dark mode on
     const body = document.body;
     const header = document.querySelector('header');
     const button = document.querySelector('#btn');
     const word = document.querySelector('#All');
+    const list = document.querySelector('#suggestion-list');
 
+    // Toggle the 'dark-mode' class on the elements
     body.classList.toggle('dark-mode');
     header.classList.toggle('dark-mode');
     button.classList.toggle('dark-mode');
     word.classList.toggle('dark-mode');
+    list.classList.toggle('dark-mode');
     
     // Store the user's preference in local storage
     if (body.classList.contains('dark-mode')) {
@@ -21,7 +25,7 @@ function toggleDarkMode() {
 // Check user's preference from local storage
 const storedTheme = localStorage.getItem('dark-mode');
 if (storedTheme === 'enabled') {
-    toggleDarkMode();
+    toggleDarkMode(); // Enable dark mode if user preference is 'enabled'
 }
 
 // Add click event listener to the theme toggle button
